ab926daab9d63d828e34418127eede7fb591bc86
rishtansoft <rishtansoft@gmail.com>
2024-04-24 10:12:27 +0500

    all endoint created

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const machines = require('./cars.json');
+const cors = require('cors');
+
+app.use(cors())
+
 
 app.get('/machines', (req, res) => {
     const page = parseInt(req.query.page) || 1;
@@ -31,6 +35,11 @@ app.get('/machines', (req, res) => {
     results.total = total;
     res.json(results);
 });
+
+app.get('/all', (req, res) => {
+     
+     res.json(machines);
+ });
 
 
 const PORT = process.env.PORT || 3000;
