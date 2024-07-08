9241bfe60c8c58994dfb3ef867242b12cf2cf658
rishtansoft <rishtansoft@gmail.com>
2024-07-08 16:38:34 +0500

    products added

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const machines = require('./cars.json');
 const countries = require('./countries.json');
+const products = require('./products.json');
 const cors = require('cors');
 
 app.use(cors())
-
 
 app.get('/machines', (req, res) => {
     const page = parseInt(req.query.page) || 1;
@@ -69,8 +69,45 @@ app.get('/countries', (req, res) => {
 
 app.get('/all-countries', (req, res) => {
      res.json(countries);
- });
+});
 
+app.get('/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/products/filter', (req, res) => {
+    const { minPrice, maxPrice } = req.query;
+
+    // Narxlarni raqamga aylantirish
+    const min = parseInt(minPrice, 10);
+    const max = parseInt(maxPrice, 10);
+
+    // Filtrlangan mahsulotlar
+    const filteredProducts = products.filter(product => product.newPrice >= min && product.newPrice <= max);
+
+    res.json(filteredProducts);
+});
+
+
+app.get('/products/category', (req, res) => {
+    const { category } = req.query;
+
+    // Filtrlangan mahsulotlar
+    const filteredProducts = products.filter(product => product.category === category);
+
+    res.json(filteredProducts);
+});
+
+app.get('/products/:id', (req, res) => {
+    const productId = parseInt(req.params.id, 10);
+    const product = products.find(p => p.id === productId);
+
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ message: 'Mahsulot topilmadi' });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
